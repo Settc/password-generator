e48ebaee468f94ff082a59b3eaeeb9aba3a08213
Thomas Whittle <tewhittle@gmail.com>
2020-10-31 09:21:12 -0400

    Add javascript file to enable functionality of pass generator

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var formControlRange = document.getElementById ("formControlRange");
+var formControlNumber = document.getElementById ("formControlNumber");
+
+formControlRange.addEventListener("input", syncRange);
+formControlNumber.addEventListener("input", syncRange);
+
+function syncRange(a) {
+     
+}
